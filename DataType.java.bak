23be20aa8493157c5f18c89e4573cd1d31e05271
Deji Adeleye <deji.adeleye@ascenz.local>
2025-10-23 11:01:13 +0100

    first Java Program

---
+++ DataType.java.bak
@@ -0,0 +1,22 @@
+public class DataType{
+	public static void main (String[] args){
+		
+		byte myAge = 25;
+		short quantity = 25000;
+		int nigerianPopulation = 200000000;
+		long worldsPopulation = 6740000000000L;
+		float price = 458.315F;
+		double balance = 3456927.674386;
+		char symbol = '$';
+		boolean IsJavaFun = true;
+		
+		
+		System.out.printf("I am %d years old%n", myAge);
+		System.out.printf("i Ordered %d quantity of phones%n", quantity);
+		System.out.printf("The Population of Nigeria is %d%n",nigerianPopulation);
+		System.out.printf("The worlds population is %d%n",worldsPopulation);
+		System.out.printf("The price for 2books is %.2f%n",price);
+		System.out.printf("My account balance is %c%.2f%n",symbol,balance);
+		System.out.printf("Is Java Fun? %b%n",IsJavaFun);
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+public class HelloWorld{
+	public static void main (String[]agrgs){
+		
+		int num = 50;
+		String name = "Jack";
+		Float deci = 9.999F;
+		boolean isFun = true;
+		char symbol = '$';
+		
+		// using println method
+		System.out.println("Hello World, this is my first java Program");
+		System.out.println("learning Java is fun");
+		
+		// using print method
+		System.out.print("The print Method should be used to accept input");
+		System.out.print("another Print Method \n");
+		
+		// using printf method
+		System.out.printf("I bought %d quantity of bread%n",num);
+		System.out.printf("My name is %s%n", name);
+		System.out.printf("Your balance is %c%.2f%n",symbol,deci);
+		System.out.printf("Is Java fun %b%n", isFun);
+		System.out.print("Do you enjoy learning \"Java\"");
+ }
+}
