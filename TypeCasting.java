7a725f88f2512cfa54ed0a56bcab005c64ac6dda
Deji Adeleye <deji.adeleye@ascenz.local>
2025-10-23 12:07:04 +0100

    Type Casting

---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting{
+	public static void main(String[] args){
+		//widening
+		double num = 10;
+		
+		System.out.printf("The value is %f%n",num);
+		
+		//Narrowing
+		double price = 34.89;
+		int result = (int)price;
+		
+		System.out.printf("The value is %d",result); 
+	}
+}
